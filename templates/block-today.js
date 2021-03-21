57a47c078b803b8807811ca4608510e9aea1c73c
Mika Vesasto <mika.vesasto@gmail.com>
2021-03-21 13:41:08 +0200

    Add namedays highlighting functionality. It will fetch userdata from Airtable

--- templates/block-today.js
+++ templates/block-today.js
@@ -8,14 +8,34 @@ const currentDate = DateTime.now()
 // Get weeknumber
 const currentWeekNumber = DateTime.local().weekNumber;
 
+// Flag template if it's a flagday
+const flagTemplate = {
+  accessory: {
+    type: 'image',
+    image_url:
+      'https://cdn.pixabay.com/photo/2013/06/08/15/08/flag-of-finland-123273_960_720.jpg',
+    alt_text: 'Liputuspäivä',
+  },
+};
+
 // This is the template block for slack message
-const todayTemplate = () => {
+const todayTemplate = (
+  currentFlagDay = '',
+  currentChangingFlagDay = '',
+  currentDayEmoji = ''
+) => {
   const template = {
     type: 'section',
     text: {
       type: 'mrkdwn',
-      text: `\n\n *Tänään on  ${currentDate} / viikko ${currentWeekNumber}. *`,
+      text: `\n\n *Tänään on  ${currentDate} / viikko ${currentWeekNumber}. *\n\n ${
+        currentFlagDay && currentFlagDay.fields.Name
+      }\n\n${
+        currentChangingFlagDay && currentChangingFlagDay.fields.Name
+      }\n\n ${currentDayEmoji && currentDayEmoji.fields.Emoji}`,
     },
+    // Include the flagday template if it's a flagday
+    ...((currentFlagDay || currentChangingFlagDay) && flagTemplate),
   };
 
   return template;
